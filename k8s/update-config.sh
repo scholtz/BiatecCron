2fe72d3faeae0047e0d4a011f3b01689e6468114
Scholtz <ludko2@gmail.com>
2024-04-14 16:57:47 +0200

    k8s deployment

---
+++ k8s/update-config.sh
@@ -0,0 +1,4 @@
+kubectl apply -f deployment-main.yaml -n biatec
+#kubectl delete configmap awallet-main-conf -n biatec
+#kubectl create configmap awallet-main-conf --from-file=conf -n biatec
+kubectl rollout restart deployment/biatec-cron-api-deployment -n biatec
