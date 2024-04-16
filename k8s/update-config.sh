cbc8a8a2d6963c212cb893b7a60463ef6709ba89
Scholtz <ludko2@gmail.com>
2024-04-16 17:15:35 +0200

    cicd deploy

--- k8s/update-config.sh
+++ k8s/update-config.sh
@@ -2,3 +2,4 @@ kubectl apply -f deployment-main.yaml -n biatec
 #kubectl delete configmap awallet-main-conf -n biatec
 #kubectl create configmap awallet-main-conf --from-file=conf -n biatec
 kubectl rollout restart deployment/biatec-cron-api-deployment -n biatec
+kubectl rollout status deployment/biatec-cron-api-deployment -n biatec
