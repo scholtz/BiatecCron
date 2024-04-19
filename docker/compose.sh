17508bc74969c068b83083d1b70fe0f5af1a7a98
Scholtz <ludko2@gmail.com>
2024-04-19 22:05:04 +0200

    executor docker image

--- docker/compose.sh
+++ docker/compose.sh
@@ -5,8 +5,8 @@ fi
 # [System.Environment]::SetEnvironmentVariable('ver','1.2024.04.07-beta')
 # docker build -t "scholtz2/biatec-cron-api:1.2024.04.07-beta" -f Dockerfile ..
 
-echo "docker build -t \"scholtz2/biatec-cron-api:$ver-main\" -f Dockerfile .."
-nice -n 20 docker build -t "scholtz2/biatec-cron-api:$ver-main" -f Dockerfile .. || error_code=$?
+echo "docker build -t \"scholtz2/biatec-cron-api:$ver-main\" -f server.Dockerfile .."
+nice -n 20 docker build -t "scholtz2/biatec-cron-api:$ver-main" -f server.Dockerfile .. || error_code=$?
 if [ "$error_code" != "" ]; then
 echo "$error_code";
     echo "failed to build";
@@ -21,3 +21,22 @@ echo "$error_code";
 fi
 
 echo "Image: scholtz2/biatec-cron-api:$ver-main"
+
+
+echo "docker build -t \"scholtz2/biatec-cron-executor:$ver-main\" -f executor.Dockerfile .."
+nice -n 20 docker build -t "scholtz2/biatec-cron-executor:$ver-main" -f executor.Dockerfile .. || error_code=$?
+if [ "$error_code" != "" ]; then
+echo "$error_code";
+    echo "failed to build";
+	exit 1;
+fi
+
+docker push "scholtz2/biatec-cron-executor:$ver-main" || error_code=$?
+if [ "$error_code" != "" ]; then
+echo "$error_code";
+    echo "failed to push";
+	exit 1;
+fi
+
+echo "Image: scholtz2/biatec-cron-executor:$ver-main"
+
