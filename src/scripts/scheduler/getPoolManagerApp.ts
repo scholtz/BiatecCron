eb1e00e204afd5a6dbc003653564cc918ce1d5d4
Scholtz <ludko2@gmail.com>
2024-04-20 19:45:18 +0200

    deploy voitest, mainnet

--- src/scripts/scheduler/getPoolManagerApp.ts
+++ src/scripts/scheduler/getPoolManagerApp.ts
@@ -1,11 +1,11 @@
 const getPoolManagerApp = (env: string) => {
   switch (env) {
     case 'mainnet-v1.0':
-      return 0;
+      return 1765620242;
     case 'testnet-v1.0':
       return 643872805;
     case 'voitest-v1':
-      return 0;
+      return 40433700;
     case 'sandnet-v1':
     default:
       return 1114;
