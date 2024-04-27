61f89299efc56d7ec798674173f481b4c73ef9f9
Ludovit Scholtz <ludko2@gmail.com>
2024-04-27 16:01:27 +0200

    biatec-scheduler@1.0.1 - BiatecTaskManagerClient, BiatecCronJobShortHashClient

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import getBoxReferenceUser from './scripts/scheduler/getBoxReferenceUser';
 import getPoolManagerApp from './scripts/scheduler/getPoolManagerApp';
 import parseBoxData from './scripts/scheduler/parseBoxData';
 
+import { BiatecTaskManagerClient } from '../contracts/clients/BiatecTaskManagerClient';
+import { BiatecCronJobShortHashClient } from '../contracts/clients/BiatecCronJob__SHORT_HASH__Client';
+
 export { getBoxReferenceApp, getBoxReferenceUser, getPoolManagerApp, parseBoxData };
+export { BiatecTaskManagerClient, BiatecCronJobShortHashClient };
